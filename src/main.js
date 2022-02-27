ed8d87bb3b9e8b93162cc8dc7e7a5a1df34d5e83
PatoGordo <icariusv@gmail.com>
2022-02-27 20:07:58 -0300

    adding app own navbar

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+let isNavOpened = false
+
+document.querySelector("#toggler").addEventListener("click", () => {
+  document.querySelector("#navbar").classList[isNavOpened? "remove" : "add"]("open")
+  
+  if (isNavOpened) {
+    document.querySelector("#mobile-navbar").style.top = '-500px'
+    isNavOpened = false
+  } else {
+    document.querySelector("#mobile-navbar").style.top = '60px'
+    isNavOpened = true
+  }
+})
+
+document.querySelector("#mobile-navbar").addEventListener("click", () => {
+  document.querySelector("#navbar").classList[isNavOpened? "remove" : "add"]("open")
+  document.querySelector("#mobile-navbar").style.top = '-500px'
+  isNavOpened = false
+})
+
 const colors = ["white", "black", "blue", "green", "red", "orange", "pink", "purple",]
 
 let css = ''
