ed017da876851e499d8bf0e1f8cf6e5c11efcaaf
PatoGordo <icariusv@gmail.com>
2022-03-01 17:18:28 -0300

    changing all background-color to just background

--- src/main.js
+++ src/main.js
@@ -7,15 +7,15 @@ Promise.all([
     // GENERATE THE LESSCSS COLORS CODE 
     /*
     css += `\n\n.c-${color} {color: @${color} !important;}`
-    css += `\n.bg-${color} {background-color: @${color} !important;}`
-    css += `\n.border-${color} {border-color: @${color} !important;} button.outlined.border-${color} {&:hover {background-color: @${color} !important; color: @white !important; filter: none;} color: @${color} !important;}`
-    css += `\n.switch {input {&:checked+.slider {&.${color} {background-color: @${color} !important;}&.${color} {box-shadow: 0 0 1px @${color} !important;}}}}`
+    css += `\n.bg-${color} {background: @${color} !important;}`
+    css += `\n.border-${color} {border-color: @${color} !important;} button.outlined.border-${color} {&:hover {background: @${color} !important; color: @white !important; filter: none;} color: @${color} !important;}`
+    css += `\n.switch {input {&:checked+.slider {&.${color} {background: @${color} !important;}&.${color} {box-shadow: 0 0 1px @${color} !important;}}}}`
 
     if (color !== "black" && color !== "white") {
       css += `\n.c-deep-${color} {color: @deep-${color} !important;}`
-      css += `\n.bg-deep-${color} {background-color: @deep-${color} !important;}`
-      css += `\n.border-deep-${color} {border-color: @deep-${color} !important;} button.outlined.border-deep-${color} {&:hover {background-color: @deep-${color} !important; color: @white !important; filter: none;} color: @deep-${color} !important;}`
-      css += `\n.switch {input {&:checked+.slider {&.deep-${color} {background-color: @deep-${color} !important;}&.deep-${color} {box-shadow: 0 0 1px @deep-${color} !important;}}}}`
+      css += `\n.bg-deep-${color} {background: @deep-${color} !important;}`
+      css += `\n.border-deep-${color} {border-color: @deep-${color} !important;} button.outlined.border-deep-${color} {&:hover {background: @deep-${color} !important; color: @white !important; filter: none;} color: @deep-${color} !important;}`
+      css += `\n.switch {input {&:checked+.slider {&.deep-${color} {background: @deep-${color} !important;}&.deep-${color} {box-shadow: 0 0 1px @deep-${color} !important;}}}}`
     }
     */
 
